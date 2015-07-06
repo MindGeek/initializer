f2561deeb624f01f7e828525ccf078a55b203c05
baidu <baidu@MacBook-Pro.local>
2015-07-07 06:19:04 +0800

    add deploy tools and origal .vimrc

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
 # This script try to deplay things onto your machine.
+
+work_dir=$(dirname $0)
+home_dir=$(cd ~ && pwd)
+deplay_source_dir=source
+
+
+function deplay()
+{
+	src=$1
+	tar=$2
+	cmd="cp $deplay_source_dir/$src $tar"
+	echo $cmd
+	eval $cmd
+	
+}
+
+# vim
+deplay source.vimrc $home_dir/.vimrc
