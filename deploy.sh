6c395f994e7ba7cb7636c9d817c558dd4b3a024a
baidu <baidu@MacBook-Pro.local>
2015-07-07 17:50:32 +0800

    change deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,16 +6,65 @@ work_dir=$(dirname $0)
 home_dir=$(cd ~ && pwd)
 deplay_source_dir=source
 
-
 function deplay()
 {
-	src=$1
-	tar=$2
+	local src=$1
+	local tar=$2
 	cmd="cp $deplay_source_dir/$src $tar"
-	echo $cmd
+	log_notice $cmd
 	eval $cmd
-	
 }
 
+function deplay_one_file()
+{
+	local tar_name="$@"
+	local tar="$home_dir/$tar_name"
+
+	if [ ! -f $tar ]
+	then
+		deplay $tar_name $tar
+		log_notice "deplay $tar done."
+	else
+		log_warning "$tar already exists, I won't cover it!"
+	fi
+}
+
+
+function log_warning()
+{
+	msg="$@"	
+	echo "WARNING:	$msg"
+}
+
+function log_fatal()
+{
+	msg="$@"
+	echo "FATAL:	$msg"
+	exit 1
+}
+
+function log_notice()
+{
+	msg="$@"
+	echo "NOTICE:	$msg"
+}
+
+
+#########################
+# deplay
+#########################
+
+# bash
+deplay_one_file .bashrc
+
 # vim
-deplay source.vimrc $home_dir/.vimrc
+deplay_one_file .vimrc
+
+
+#########################
+# source
+#########################
+source $home_dir/.bashrc
+
+
+
